39a701cd129412e405d2c3efb4148bcf7d759661
samokhinv <valik.samokhin@gmail.com>
2020-11-25 01:43:38 +0300

    InfoNCE implementation

--- cpc/nce.py
+++ cpc/nce.py
@@ -9,6 +9,19 @@ class InfoNCE(nn.Module):
         pass
 
     @staticmethod
-    def forward(positive, negative):
-        probs = log_softmax(torch.stack((positive, negative), -1), -1)  # TODO: there is a simplier formula that doesn't require to compute all probabilities
-        return probs[...:-1]
+    def forward(predicted_samples, encoded_samples):
+        """
+        predicted_samples - next sequences, predicted from context representation
+        encoded_samples - golden "next" sentences, passed through encoder
+
+        returns mean info-nce loss per batch
+        """
+
+        scores = torch.mm(predicted_samples, encoded_samples.transpose(0, 1))  # (batch, enc_len) x (batch, enc_len) -> (batch, batch)
+
+        """
+        Scores is a matrix: s_{ij} is a dot-product of i-th predicted sample and j-th sample from random samples.
+        E_i is a positive sample for P_i; E_j, j != i, is considered as a negative sample for P_i
+        """
+        logprobs = log_softmax(scores, 1)
+        return torch.diagonal(logprobs).mean()
