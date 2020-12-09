0cafdd0d5c9e6c6bbfc07129dbc0a466f2e39556
samokhinv <valik.samokhin@gmail.com>
2020-12-10 01:36:07 +0300

    reshape_before_ar parameter for CPC plModule

--- scripts/cpc/pl_model.py
+++ scripts/cpc/pl_model.py
@@ -8,18 +8,17 @@ import torch
 
 
 class CPCModel(pl.LightningModule):
-    def __init__(self, vocab_size, emb_dim, enc_dim, ar_dim, kernel_size, lr, empty_cache=True):
+    def __init__(self, vocab_size, emb_dim, enc_dim, ar_dim, kernel_size, lr, reshape_before_ar=1):
         super(CPCModel, self).__init__()
         self.save_hyperparameters()
         self.cpc = CPC(vocab_size=vocab_size,
                        emb_dim=emb_dim,
                        enc_dim=enc_dim,
                        ar_dim=ar_dim,
-                       kernel_size=kernel_size)
+                       kernel_size=kernel_size,
+                       reshape_before_ar=reshape_before_ar)
 
         self.lr = lr
-
-        self.empty_cache = empty_cache
 
     def configure_optimizers(self):
         return Adam(self.parameters(), lr=self.lr)
@@ -31,8 +30,7 @@ class CPCModel(pl.LightningModule):
         text_batch = batch
         info_nce_loss = self.cpc(text_batch)
         self.log('info_nce', info_nce_loss)
-        if self.empty_cache:
-            torch.cuda.empty_cache()
+
         return info_nce_loss
 
     def validation_step(self, batch, batch_idx):
@@ -40,5 +38,3 @@ class CPCModel(pl.LightningModule):
         info_nce_loss = self.cpc(text_batch)
 
         self.log('val_info_nce', info_nce_loss)
-        if self.empty_cache:
-            torch.cuda.empty_cache()
